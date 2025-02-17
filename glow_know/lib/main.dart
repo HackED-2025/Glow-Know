165161a5844d580ed1a611553d7d5943f944bc20
Mihiri Kamiss <76460057+Jaymerist@users.noreply.github.com>
2025-02-16 21:33:29 -0700

    I hate losing monopoly deal

--- glow_know/lib/main.dart
+++ glow_know/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Vespera',
+      title: 'INGREDIA',
       theme: ThemeData(
         scaffoldBackgroundColor: AppColors.background,
         fontFamily: 'Nunito',
